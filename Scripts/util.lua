5fa13891a47f816004cad16c254558fb4665ce6f
Wisam Ghantous <welwisam@gmail.com>
2017-03-19 14:12:48 +0100

    Add tmr util funcs

--- Scripts/util.lua
+++ Scripts/util.lua
@@ -11,3 +11,14 @@ function tableMedian(tIn)
     indx = math.floor(#tIn / 2) + 1
     return tIn[indx];
 end
+
+function utilTmrCreate(mode, delayMs, func)
+    local ltmr = tmr.create()
+    ltmr:register(delayMs, mode, func)
+    ltmr:start()
+    return ltmr
+end
+
+function tmrDelay(delayMs, func) return utilTmrCreate(tmr.ALARM_SINGLE, delayMs, func) end
+function tmrSemi(delayMs, func) return utilTmrCreate(tmr.ALARM_SEMI, delayMs, func) end
+function tmrRepeat(delayMs, func) return utilTmrCreate(tmr.ALARM_AUTO, delayMs, func) end
