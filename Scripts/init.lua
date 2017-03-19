b06bcaf86b977f7d961f017ab9d4e392593ff673
Wisam Ghantous <welwisam@gmail.com>
2017-03-19 14:34:49 +0100

    Add 'init.lua'
    Which will connect to internetz on startup. Also causes fancy LED
    flickering!

---
+++ Scripts/init.lua
@@ -0,0 +1,40 @@
+print('\r\n-- start of init.lua --\r\n')
+
+dofile('util.lua')
+dofile('leds.lua')
+
+wifi.setmode(wifi.STATION)
+
+ssid = 'Do not upload to GitHub'
+pwd = 'Do not upload to GitHub'
+
+wifi.sta.sethostname("ESP8266-WG")
+wifi.sta.config(ssid, pwd)
+wifi.sta.connect()
+
+tryCount = 0
+conTmr = tmrRepeat(500, function()
+    if wifi.sta.getip()== nil then
+        print("Not yet connected...")
+        flash(lRed,100)
+    else
+        print('\r\n-- Connection established --')
+        print("  Mode: " .. wifi.getmode())
+        print("  MAC: " .. wifi.ap.getmac())
+        print("  IP: "..wifi.sta.getip())
+        flash(lGrn,1000)
+        conTmr:unregister()
+    end
+    
+    tryCount = tryCount + 1
+    if tryCount > 20 then
+        print("Connect failed, stop trying for a while.")
+        flash(lRed,1000)
+        tryCount = 0
+        wifi.sta.disconnect()
+        conTmr:stop()
+        tmrDelay(1000 * 20, function()
+            conTmr:start()
+        end)
+    end
+end)
