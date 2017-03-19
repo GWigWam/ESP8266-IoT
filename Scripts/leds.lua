21a4ce881e7c254c2a398d27a48ad600293a9f22
Wisam Ghantous <welwisam@gmail.com>
2017-03-19 14:25:26 +0100

    leds.lua now uses new util tmr func

--- Scripts/leds.lua
+++ Scripts/leds.lua
@@ -8,7 +8,7 @@ gpio.mode(lRed,gpio.OUTPUT)
 
 function flash(led, durationMs)
     gpio.write(led,gpio.HIGH)
-    tmr.create():alarm(durationMs, tmr.ALARM_SINGLE, function()
+    tmrDelay(durationMs, function()
         gpio.write(led,gpio.LOW)
     end)
 end
