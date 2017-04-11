a522daf39d0935e1aa44914c6be10eb33d19984b
Wisam Ghantous <welwisam@gmail.com>
2017-04-11 20:40:24 +0200

    Add blue LED
    Also added a 100ohm resistor, but I can hardly commit that to GitHub.

--- Scripts/leds.lua
+++ Scripts/leds.lua
@@ -2,9 +2,12 @@ print("init 'leds.lua'")
 
 lGrn = 1
 lRed = 2
-
-gpio.mode(lGrn,gpio.OUTPUT)
-gpio.mode(lRed,gpio.OUTPUT)
+lBlu = 3
+lLeds = {lGrn,lRed,lBlu}
+table.foreach(lLeds,function(k,v)
+    gpio.mode(v,gpio.OUTPUT)
+    gpio.write(v,gpio.LOW)
+end)
 
 function flash(led, durationMs)
     gpio.write(led,gpio.HIGH)
