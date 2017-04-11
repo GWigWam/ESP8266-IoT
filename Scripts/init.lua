96eb1890e442910d1c5c77096fcedb144c7480b6
Wisam Ghantous <welwisam@gmail.com>
2017-04-11 22:07:25 +0200

    Simplify 'init.lua'

--- Scripts/init.lua
+++ Scripts/init.lua
@@ -6,54 +6,27 @@ dofile('leds.lua')
 dofile('dht.lua')
 -- Sets ssid and pwd vars, also sets ThingSpeak private write key
 dofile('secret.lua')
+dofile('wifiEvents.lua')
 
-function setMac() if not wifi.sta.setmac("AA:AA:AA:E5:82:66") then log('set MAC failed') end end
+dofile("ThingSpeakTemp.lua")
+startTSService()
+dofile("statusHost.lua")
+createStatusHost()
 
-function onConnect() --Shall run when connection is first established
-    setMac()
-    wifi.sta.autoconnect(1)
-    wifi.sta.eventMonReg(wifi.STA_WRONGPWD,function()log("STATION_WRONG_PASSWORD")end)
-    wifi.sta.eventMonReg(wifi.STA_APNOTFOUND,function()log("STATION_NO_AP_FOUND")end)
-    wifi.sta.eventMonReg(wifi.STA_FAIL,function()log("STATION_CONNECT_FAIL")end)
-    wifi.sta.eventMonStart()
-
-    dofile("ThingSpeakTemp.lua")
-    startTSService()
-
-    dofile("statusHost.lua")
-    createStatusHost()
-
-    startLogBackup()
-end
+startLogBackup()
 
 wifi.setmode(wifi.STATION)
 wifi.sta.sethostname("ESP8266-WG")
 wifi.sta.config(ssid, pwd)
-setMac()
+wifi.sta.autoconnect(1)
+if not wifi.sta.setmac("AA:AA:AA:E5:82:66") then log('set MAC failed') end
 wifi.sta.connect()
 
 tryCount = 0
-conTmr = tmrRepeat(500, function()
+tmrSemi(1000, function(conTmr)
     if wifi.sta.getip()== nil then
-        print("Not yet connected...")
-        flash(lRed,50)
-    else
-        log('\r\n-- Connection established --')
-        log('{ "Mode":"'..wifi.getmode()..'", "MAC":"'..wifi.ap.getmac()..'", "IP":"'..wifi.sta.getip()..'", "CH":"'..wifi.getchannel()..'" }')
-        flash(lGrn,1000)
-        conTmr:unregister()
-        onConnect()
-    end
-
-    tryCount = tryCount + 1
-    if tryCount > 20 then
-        log("Connect failed, stop trying for a while.")
-        flash(lRed,1000)
-        tryCount = 0
-        wifi.sta.disconnect()
-        conTmr:stop()
-        tmrDelay(1000 * 20, function()
-            conTmr:start()
-        end)
-    end
+        flash(lRed,100)
+        tryCount = tryCount + 1
+        if tryCount < 20 then tryCount = tryCount +1; conTmr:start() else conTmr:unregister() end
+    else conTmr:unregister() end
 end)
