46eb5329a34b7ace654abcedf9bd3ed1a8c00f3a
Wisam Ghantous <welwisam@gmail.com>
2017-05-11 19:51:40 +0200

    Restart DHT sensor on read failure
    To achieve this DHT-pin1 (+) is now connected to GPIO pin 5, instead of
    3V source.

--- Scripts/dht.lua
+++ Scripts/dht.lua
@@ -1,4 +1,9 @@
 dhtPin = 4
+dhtPwrPin = 5
+
+gpio.mode(dhtPwrPin,gpio.OUTPUT)
+gpio.write(dhtPwrPin,gpio.HIGH)
+
 function getDHTStats()
     status, temp, humi, temp_dec, humi_dec = dht.read(dhtPin)
     if status == dht.OK then
@@ -9,7 +14,13 @@ function getDHTStats()
         elseif status == dht.ERROR_TIMEOUT then
             log("DHT timed out.")
         end
+        restartDHT()
         flash(lRed,1000)
         return false, -999, -999
     end 
+end
+
+function restartDHT()
+    gpio.write(dhtPwrPin,gpio.LOW)
+    tmrDelay(1000,function() gpio.write(dhtPwrPin,gpio.HIGH) end)
 end
