654d848da3e013594160dfd42b8adacfce867e97
GWigWam <welwisam@gmail.com>
2018-01-29 21:56:58 +0100

    Return LCD consts in table instead of setting globals

--- Scripts/lcd/lcd1602.lua
+++ Scripts/lcd/lcd1602.lua
@@ -1,5 +1,5 @@
 -- lcd1602.lua by GWigWam
-dofile("lcd1602_const.lua");
+const = dofile("lcd1602_const.lua");
 print "'lcd1602.lua'"
 
 backlight = true
@@ -12,28 +12,28 @@ function begin() -- As specified on page 46 of HD44780U specsheet
     send(0x30)
     tmr.delay(120) -- 100ns
 
-    local func = bit.bor(LCD_4BITMODE, LCD_2LINE, LCD_5x8DOTS)
-    local cntr = bit.bor(LCD_DISPLAYON, LCD_CURSORON, LCD_BLINKON)    
-    local mode = bit.bor(LCD_ENTRYLEFT, LCD_ENTRYSHIFTDECREMENT)
+    local func = bit.bor(const["4BITMODE"], const["2LINE"], const["5x8DOTS"])
+    local cntr = bit.bor(const["DISPLAYON"], const["CURSORON"], const["BLINKON"])    
+    local mode = bit.bor(const["ENTRYLEFT"], const["ENTRYSHIFTDECREMENT"])
 
-    send({ bit.bor(LCD_FUNCTIONSET, LCD_4BITMODE) }) -- Set 4-bit mode
-    send({ LCD_FUNCTIONSET, bit.lshift(func, 4) }) -- Function set (NoLines, Font)
-    send({ 0x00, bit.lshift(bit.bor(LCD_DISPLAYCONTROL, cntr), 4) }) -- Control set (On/off, Cursor, Blink)
+    send({ bit.bor(const["FUNCTIONSET"], const["4BITMODE"]) }) -- Set 4-bit mode
+    send({ const["FUNCTIONSET"], bit.lshift(func, 4) }) -- Function set (NoLines, Font)
+    send({ 0x00, bit.lshift(bit.bor(const["DISPLAYCONTROL"], cntr), 4) }) -- Control set (On/off, Cursor, Blink)
     clear()
-    send({ 0x00, bit.lshift(bit.bor(LCD_ENTRYMODESET, mode), 4) }) --Entry mode set (Align, Disp. shift)
+    send({ 0x00, bit.lshift(bit.bor(const["ENTRYMODESET"], mode), 4) }) --Entry mode set (Align, Disp. shift)
     
     tmr.delay(2000)
 end
 
 function clear()
-    send({0x00, bit.lshift(LCD_CLEARDISPLAY, 4)})
+    send({0x00, bit.lshift(const["CLEARDISPLAY"], 4)})
     tmr.delay(2000)
 end
 
-function setCursor(col, row)
+function setCursor(row, col)
     local row_offsets = { 0x00, 0x40 }
     local offset = row_offsets[row + 1]
-    send({ bit.bor(LCD_SETDDRAMADDR, offset), bit.lshift(col, 4) })
+    send({ bit.bor(const["SETDDRAMADDR"], offset), bit.lshift(col, 4) })
 end
 
 function sendStr(str)
@@ -49,8 +49,8 @@ end
 
 function send(vals, rs)
     if type(vals) ~= "table" then vals = {vals} end
-    local blBit = backlight and LCD_BACKLIGHT or LCD_NOBACKLIGHT
-    local rsBit = rs and RsBit or 0x00
+    local blBit = backlight and const["BACKLIGHT"] or const["NOBACKLIGHT"]
+    local rsBit = rs and const["RsBit"] or 0x00
 
     local debStr = string.format("snd (%i):", #vals)
     for i = 1, #vals do
@@ -61,7 +61,7 @@ function send(vals, rs)
     local tbl = { 0x00 } -- TODO, might not be necisary, use '{ }' instead
     for i = 1, #vals do
         local payl = bit.bor(vals[i], blBit, rsBit)
-        table.insert(tbl, bit.bor(payl, EnBit))
+        table.insert(tbl, bit.bor(payl, const["EnBit"]))
         table.insert(tbl, payl)
     end
     i2cw(tbl)
