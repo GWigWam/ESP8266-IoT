cc80b1f710f7558dbed8d8219f0ccb09323cafc4
GWigWam <welwisam@gmail.com>
2018-10-01 20:52:13 +0200

    Take i2c pins as params

--- Scripts/i2c/i2c.lua
+++ Scripts/i2c/i2c.lua
@@ -1,17 +1,16 @@
 print("'i2c.lua'")
 
-sda = 2
-scl = 1
-adr = 0x3F
-i2cdebug = true
+i2cdebug = false
+local i2cAdr = 0x00;
 
-function i2cinit()
+function i2cinit(sda, scl, adr)
+    i2cAdr = adr;
     i2c.setup(0, sda, scl, i2c.SLOW)
 end
 
 function i2cw(data)
     i2c.start(0)
-    i2c.address(0, adr, i2c.TRANSMITTER)
+    i2c.address(0, i2cAdr, i2c.TRANSMITTER)
     w = i2c.write(0, data)
     i2c.stop(0)
 
