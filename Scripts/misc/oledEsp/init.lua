a59ca559d345a220e94dbd62bc1cd1e598f5fe75
GWigWam <welwisam@gmail.com>
2018-10-17 21:30:09 +0200

    Sync time w/ nist svc on startup

--- Scripts/misc/oledEsp/init.lua
+++ Scripts/misc/oledEsp/init.lua
@@ -4,6 +4,21 @@ local sda = 2
 local scl = 1
 local i2cAdr = 0x3C
 
+local function initWifi()
+    dofile('secret.lua')
+    wifi.setmode(wifi.STATION)
+    wifi.sta.config({ssid = ssid, pwd = pwd, auto = true})
+    wifi.eventmon.register(wifi.eventmon.STA_GOT_IP, function(T)
+        print("GOT_IP: "..T.IP)
+        net.dns.resolve("time.nist.gov", function(sk, ip)
+            if (ip == nil) then print("DNS fail!") else
+                print("Got nist IP: "..ip)
+                sntp.sync(ip, function(sec, ms, srv, inf) print("Got UTC, Sec: "..sec) end, nil, true)
+            end
+        end)
+    end)
+end
+
 local function init()
     if not stopInit then
         print("Starting...")
@@ -16,8 +31,11 @@ local function init()
         local i2cw = require("i2cwriter")
         i2cw.init(sda, scl, i2cAdr)
         oled = require("SSD1306")
+        oled:init()
         gfx = require("gfx")
         font = require("gfx_font")
+        
+        initWifi()
     end
 end
 
